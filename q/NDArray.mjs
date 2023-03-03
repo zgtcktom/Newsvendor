39862f229c59f143832d6977e71a355417c33360
zgtcktom <ckt.tomchung@gmail.com>
2023-03-03 18:15:28 +0800

    module

--- q/NDArray.mjs
+++ q/NDArray.mjs
@@ -1,199 +1,18 @@
-import { Tester } from './Tester.mjs';
+import { tester } from './core/core.mjs';
+import { slice, isscalar } from './core/core.mjs';
+import { NDArray, array, shape, broadcast_to, copyto, ndindex, ndoffset } from './core/core.mjs';
+import { ndim } from './core/numeric.mjs';
 
-let tester = new Tester();
-
-class Slicer {
-    constructor(start, stop, step) {
-        this.start = start;
-        this.stop = stop;
-        this.step = step;
-    }
-    get(length) {
-        // https://svn.python.org/projects/python/branches/pep-0384/Objects/sliceobject.c
-        let { start, stop, step } = this;
-        if (step == null) {
-            step = 1;
-        }
-
-        let defstart = step < 0 ? length - 1 : 0;
-        let defstop = step < 0 ? -1 : length;
-
-        if (start == null) {
-            start = defstart;
-        } else {
-            if (start < 0) start += length;
-            if (start < 0) start = step < 0 ? -1 : 0;
-            if (start >= length) start = step < 0 ? length - 1 : length;
-        }
-
-        if (stop == null) {
-            stop = defstop;
-        } else {
-            if (stop < 0) stop += length;
-            if (stop < 0) stop = step < 0 ? -1 : 0;
-            if (stop >= length) stop = step < 0 ? length - 1 : length;
-        }
-
-        let slicelength;
-        if ((step == 0) || (step < 0 && stop >= start) || (step > 0 && start >= stop)) {
-            slicelength = 0;
-        } else if (step < 0) {
-            slicelength = (stop - start + 1) / (step) + 1 | 0;
-        } else {
-            slicelength = (stop - start - 1) / (step) + 1 | 0;
-        }
-        return new Slice(start, stop, step, slicelength);
-    }
-}
-
-function slice(start, stop, step) {
-    if (is_tuple(start)) {
-        ({ 0: start, 1: stop, 2: step } = start);
-    }
-    return new Slicer(start, stop, step);
-}
-
-class Slice {
-    constructor(start, stop, step, slicelength) {
-        this.start = start;
-        this.stop = stop;
-        this.step = step;
-        this.slicelength = slicelength;
-    }
-}
-
-
-function get_strides(shape, itemsize) {
-    let strides = [];
-    if (shape.length > 0) {
-        strides[shape.length - 1] = itemsize;
-        for (let i = shape.length - 2; i >= 0; i--) {
-            strides[i] = strides[i + 1] * shape[i + 1];
-        }
-    }
-    return strides;
-}
-
-function get_size(shape) {
-    if (shape.length == 0) return 0;
-    let size = 1;
-    for (let n of shape) size *= n;
-    return size;
-}
+let runTest = true;
 
 function is_int(value) {
-    return Number.isInteger(value);
-}
-
-function is_tuple(value) {
-    return value?.length != undefined;
-}
-
-function type(value) {
-    return value?.constructor.name;
+	return Number.isInteger(value);
 }
 
 function prod(a) {
-    let prod = 1;
-    for (let n of a) prod *= n;
-    return prod;
-}
-
-export class NDArray {
-    constructor(shape, buffer = null, strides = get_strides(shape, 1), offset = 0, itemsize = 1) {
-        // https://numpy.org/doc/stable/reference/generated/numpy.ndarray.html
-        this.size = get_size(shape);
-        this.ndim = shape.length;
-        this.base = null;
-
-        this.shape = shape;
-        this.data = buffer ?? new Array(this.size);
-        this.itemsize = itemsize;
-        this.strides = strides;
-        this.offset = offset;
-    }
-    getitem(...indices) {
-        let { shape, data, itemsize, strides, offset } = this;
-        let ndim = 0;
-        if (indices.length > shape.length) throw 'too many indices for array';
-        for (let index of indices) {
-            if (shape.length == 0) throw 'invalid index to scalar variable';
-            if (index instanceof Slicer) {
-                let { start, stop, step, slicelength } = index.get(shape[ndim]);
-                // console.log('index', start, stop, step, slicelength)
-                offset = offset + strides[ndim] * start;
-                // console.log(slicelength)
-                shape = [...shape.slice(0, ndim), slicelength, ...shape.slice(ndim + 1)];
-                strides = [...strides.slice(0, ndim), strides[ndim] * step, ...strides.slice(ndim + 1)];
-                ndim++;
-            } else {
-                if (index < 0) index += shape[ndim];
-                offset = offset + strides[ndim] * index;
-                shape = [...shape.slice(0, ndim), ...shape.slice(ndim + 1)];
-                strides = [...strides.slice(0, ndim), ...strides.slice(ndim + 1)];
-            }
-        }
-        return new NDArray(shape, data, strides, offset, itemsize);
-    }
-    item(index) {
-        if (index == undefined) {
-            let { size } = this;
-            if (size != 1) throw 'index cannot be empty if size != 1';
-            index = 0;
-        }
-        if (is_int(index)) {
-            let { ndim, shape, size } = this;
-            if (index >= size) throw `index ${index} out of bound for size ${size}`;
-            let sizes = [...shape];
-            for (let i = sizes.length - 2; i >= 1; i--) {
-                sizes[i] *= sizes[i + 1];
-            }
-            let indices = Array(ndim).fill(0);
-            for (let i = 0; i < indices.length - 1; i++) {
-                let size = sizes[i + 1];
-                if (index >= size) {
-                    indices[i] = index / size | 0;
-                    index %= size;
-                }
-            }
-            indices[indices.length - 1] = index;
-            // console.log('sizes', sizes, indices);
-            index = indices;
-        } else if (!is_tuple(index)) throw `unexpected type '${type(index)}'`;
-
-        let { data, strides, shape, offset } = this;
-        for (let i = 0; i < index.length; i++) {
-            offset += (index[i] < 0 ? index[i] + shape[i] : index[i]) * strides[i];
-        }
-        return data[offset];
-
-    }
-    toarray() {
-        let { ndim, offset, data, shape } = this;
-        if (ndim == 0) return data[offset];
-        let array = [];
-        for (let i = 0; i < shape[0]; i++) {
-            array.push(this.getitem(i).toarray());
-        }
-        return array;
-    }
-}
-
-function* ndindex(shape, reuse = true) {
-    let index = Array(shape.length).fill(0);
-    let size = prod(shape);
-    if (size == 0) return;
-
-    yield reuse ? index : index.slice();
-    for (let i = 1; i < size; i++) {
-        for (let j = index.length - 1; j >= 0; j--) {
-            index[j] += 1;
-            if (index[j] < shape[j])
-                break;
-            index[j] -= shape[j];
-        }
-        yield reuse ? index : index.slice();
-    }
+	let prod = 1;
+	for (let n of a) prod *= n;
+	return prod;
 }
 
 // for (let i of ndindex([3, 2, 1])) {
@@ -202,126 +21,200 @@ function* ndindex(shape, reuse = true) {
 // console.log([...ndindex([3, 2, 1], false)]);
 
 export function broadcast_shapes(...shapes) {
-    let ndim = 0;
-    for (let shape of shapes) ndim = Math.max(ndim, shape.length);
-    if (ndim == 0) return [];
+	let ndim = 0;
+	for (let shape of shapes) ndim = Math.max(ndim, shape.length);
+	if (ndim == 0) return [];
 
-    let broadcasted = Array(ndim).fill(1);
-    for (let shape of shapes) {
-        for (let i = shape.length - 1, j = ndim - 1; i >= 0; i--, j--) {
-            let dim = shape[i];
-            if (dim == 1) continue;
-            if (broadcasted[j] == 1) broadcasted[j] = dim;
-            else if (broadcasted[j] != dim) throw 'shape mismatch';
-        }
-    }
-    return broadcasted;
+	let broadcasted = Array(ndim).fill(1);
+	for (let shape of shapes) {
+		for (let i = shape.length - 1, j = ndim - 1; i >= 0; i--, j--) {
+			let dim = shape[i];
+			if (dim == 1) continue;
+			if (broadcasted[j] == 1) broadcasted[j] = dim;
+			else if (broadcasted[j] != dim) throw 'shape mismatch';
+		}
+	}
+	return broadcasted;
 }
 
-tester.add('broadcast_shapes',
-    () => broadcast_shapes([1, 2], [3, 1], [3, 2]),
-    () => [3, 2]);
-tester.add('broadcast_shapes',
-    () => broadcast_shapes([6, 7], [5, 6, 1], [7], [5, 1, 7]),
-    () => [5, 6, 7]);
-
-export function broadcast_to(array, shape) {
-    if (array.shape.length > shape.length) throw 'broadcast shape has less dimensions than input array';
-
-    let { data, strides, offset, itemsize } = array;
-
-    let new_strides = [];
-    for (let i = shape.length - 1, j = array.shape.length - 1; i >= 0; i--, j--) {
-        if (j >= 0 && array.shape[j] != 1 && array.shape[j] != shape[i]) throw 'operands could not be broadcast together';
-        new_strides[i] = j < 0 || array.shape[j] == 1 ? 0 : strides[j];
-    }
-
-    return new NDArray(shape, data, new_strides, offset, itemsize);
-}
-
-tester.add('broadcast_to',
-    () => broadcast_to(new NDArray([3], [1, 2, 3]), [3, 3]).toarray(),
-    () => [[1, 2, 3], [1, 2, 3], [1, 2, 3]]);
-
-tester.add('broadcast_to',
-    () => broadcast_to(new NDArray([1, 3, 1], [1, 2, 3]), [2, 3, 4]).toarray(),
-    () => [[[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]]);
+tester.add(
+	'broadcast_shapes',
+	() => broadcast_shapes([1, 2], [3, 1], [3, 2]),
+	() => [3, 2]
+);
+tester.add(
+	'broadcast_shapes',
+	() => broadcast_shapes([6, 7], [5, 6, 1], [7], [5, 1, 7]),
+	() => [5, 6, 7]
+);
 
 export function iterable(obj) {
-    return obj?.[Symbol.iterator] != undefined;
+	return obj?.[Symbol.iterator] != undefined;
 }
 
-function nested_shape(array, shape, level) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i]?.length != shape[level]) {
-            shape.length = level;
-            return;
-        }
-        if (array[i]?.length != undefined && shape.length > level + 1) {
-            if (!nested_shape(array[i], shape, level + 1)) return;
-        }
-    }
-    return true;
+tester.add(
+	'shape',
+	() => shape([[[3, 9]], [[3, 9, 3]], [[3, 9]]]),
+	() => [3, 1]
+);
+
+tester.add(
+	'shape',
+	() => shape([1, 2, 3, [1, 3]]),
+	() => [4]
+);
+
+export function empty(shape) {
+	if (is_int(shape)) shape = [shape];
+	return new NDArray(shape, Array(prod(shape)));
 }
 
-export function shape(array) {
-    if (array.shape != undefined) return array.shape;
-    let shape = [];
-    let elem = array;
-    while (elem?.length != undefined) {
-        shape.push(elem.length);
-        elem = elem[0];
-    }
-    // console.log(shape.length > 1)
-    if (array.length != undefined && shape.length > 1)
-        nested_shape(array, shape, 1);
-    return shape;
+tester
+	.add(
+		'empty',
+		() => empty([2, 2]),
+		() => [
+			[, ,],
+			[, ,],
+		]
+	)
+	.add(
+		'empty',
+		() => empty(0),
+		() => []
+	)
+	.add(
+		'empty',
+		() => empty([]),
+		() => undefined
+	);
+
+export function empty_like(prototype) {
+	return empty(shape(prototype));
 }
 
-tester.add('shape',
-    () => shape([[[3, 9]], [[3, 9, 3]], [[3, 9]]]),
-    () => [3, 1]);
+tester
+	.add(
+		'empty_like',
+		() =>
+			empty_like([
+				[1, 2, 3],
+				[4, 5, 6],
+			]),
+		() => [
+			[, , ,],
+			[, , ,],
+		]
+	)
+	.add(
+		'empty_like',
+		() => empty_like([1, 2, 3, [1, 3]]),
+		() => [, , , ,]
+	);
 
-tester.add('shape',
-    () => shape([1, 2, 3, [1, 3]]),
-    () => [4]);
-
-function flatten_with_shape(data, array, shape, level = 0) {
-    if (level == shape.length) {
-        data.push(array);
-        return;
-    }
-    for (let i = 0; i < shape[level]; i++) {
-        flatten_with_shape(data, array[i], shape, level + 1);
-    }
+export function eye(N, M = N, k = 0) {
+	let size = N * M;
+	let data = Array(size).fill(0);
+	for (let i = k + Math.ceil(-k / (M + 1)) * (M + 1); i < size; i += M + 1) data[i] = 1;
+	return new NDArray([N, M], data);
 }
 
-export function array(a) {
-    let data = [];
-    flatten_with_shape(data, a, shape(a));
-    console.log(data);
+tester
+	.add(
+		'eye',
+		() => eye(2),
+		() => [
+			[1, 0],
+			[0, 1],
+		]
+	)
+	.add(
+		'eye',
+		() => eye(3, 3, 1),
+		() => [
+			[0, 1, 0],
+			[0, 0, 1],
+			[0, 0, 0],
+		]
+	)
+	.add(
+		'eye',
+		() => eye(4, 5, -1),
+		() => [
+			[0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0],
+			[0, 1, 0, 0, 0],
+			[0, 0, 1, 0, 0],
+		]
+	);
+
+export function identity(n) {
+	let size = n * n;
+	let data = Array(size).fill(0);
+	for (let i = 0; i < size; i += n + 1) data[i] = 1;
+	return new NDArray([n, n], data);
 }
 
-console.log(array([[[3, 9]], [[3, 9]], [[3, 9]]]))
+tester.add(
+	'identity',
+	() => identity(3).toarray(),
+	() => [
+		[1, 0, 0],
+		[0, 1, 0],
+		[0, 0, 1],
+	]
+);
 
-// console.log(new NDArray([2, 5], [...Array(10).keys()]).getitem(slice(), slice([, , -1])));
-// let a = new NDArray([2, 5], [...Array(10).keys()]).getitem(slice(), slice([, , -1]));
-// console.log(a.item(9));
-// console.log(type(true), type(NaN), Array(10).fill(0));
+tester.add(
+	'identity',
+	() => identity(0).toarray(),
+	() => []
+);
 
-// for (let index of ndindex(a.shape)) {
-//     console.log(index, a.item(index));
-// }
+// export function ones(shape)
+// export function ones_like(shape)
+// export function zeros(shape)
+// export function zeros_like(shape)
 
-
-export function ndarray(shape, buffer, strides, offset) {
-    return new NDArray(shape, buffer, strides, offset);
+export function full(shape, fill_value) {
+	if (isscalar(fill_value)) {
+		let data = Array(prod(shape)).fill(fill_value);
+		return new NDArray(shape, data);
+	}
+	let array = empty(shape);
+	copyto(array, fill_value);
+	return array;
 }
 
+tester
+	.add(
+		'full',
+		() => full([2, 2], Infinity),
+		() => [
+			[Infinity, Infinity],
+			[Infinity, Infinity],
+		]
+	)
+	.add(
+		'full',
+		() => full([2, 2], 10),
+		() => [
+			[10, 10],
+			[10, 10],
+		]
+	)
+	.add(
+		'full',
+		() => full([2, 2], [1, 2]),
+		() => [
+			[1, 2],
+			[1, 2],
+		]
+	);
+
+// end
 function test() {
-    console.log('test', new NDArray([3, 2, 5, 2], [...Array(60).keys()]).getitem(2, new Slicer(null, null, 2), 1).toarray());
-
-    tester.run();
+	if (runTest) tester.run();
 }
 
 test();
--- q/run_this.mjs
+++ q/run_this.mjs
@@ -5,10 +5,9 @@ import { stats, integrate, print, random, range, ndarray, DataFrame, mean, std }
 import { RL_brain_kstest } from './Q_LEARNING_kstest.mjs';
 import { RL_brain_base } from './Q_LEARNING_base.mjs';
 
-import { NDArray } from './NDArray.mjs';
+// import { NDArray } from './NDArray.mjs';
 
 // console.log(tf.add(tf.tensor([1, 2, 3]), tf.tensor([1, 2, 3])));
-
 
 let r = 0.2;
 // console.log(stats.norm(0.8, 1.2).expect(x => x - r - 1, r + 1, Infinity));
@@ -30,22 +29,20 @@ let HOLDING_COST = 1;
 let BACKLOG_COST = 20;
 
 //#Set Q learning parameter
-let EPSILON = 0.9; //# initial exploration rate 
-let ALPHA = 0.6;     //# initial learning rate
+let EPSILON = 0.9; //# initial exploration rate
+let ALPHA = 0.6; //# initial learning rate
 let GAMMA = 0.1; //# discount factor
 let N_STATES = 121; //#Define the inventory position space
-let ACTIONS = 40;//#Define the largerst reorder quantity
-let SIMULATION_TIME = 100;//#number of iteration
+let ACTIONS = 40; //#Define the largerst reorder quantity
+let SIMULATION_TIME = 100; //#number of iteration
 
 // #Set action space
-// #1 represents uniform action space; 
-// #2 represents base stock action space; 
+// #1 represents uniform action space;
+// #2 represents base stock action space;
 // #3 represents safty stock action space
 let ACTION_NUMBER = 3;
 let upperlimit_constant = 10;
 // # Set learning method
-
-
 
 // let method = new RL_brain_kstest(MEAN1, VAR1, MEAN2, VAR2, LEADTIME,
 //     HOLDING_COST, BACKLOG_COST,
@@ -78,175 +75,177 @@ let upperlimit_constant = 10;
 
 // console.log(R_episode);
 
-
 function binarysearch(a, v) {
-    let left = 0, right = a.length;
-    // if (v < a[left]) return 0;
-    // if (v > a[right - 1]) return right;
-    while (left <= right) {
-        let middle = (left + right) / 2 | 0;
-        // console.log(middle, left, right, ale], v);
-        if (a[middle] == v) return middle;
-        if (a[middle] < v) {
-            left = middle + 1;
-        } else {
-            right = middle - 1;
-        }
-    }
-    let middle = (left + right) / 2 | 0;
-    return v < a[middle] ? middle : middle + 1;
+	let left = 0,
+		right = a.length;
+	// if (v < a[left]) return 0;
+	// if (v > a[right - 1]) return right;
+	while (left <= right) {
+		let middle = ((left + right) / 2) | 0;
+		// console.log(middle, left, right, ale], v);
+		if (a[middle] == v) return middle;
+		if (a[middle] < v) {
+			left = middle + 1;
+		} else {
+			right = middle - 1;
+		}
+	}
+	let middle = ((left + right) / 2) | 0;
+	return v < a[middle] ? middle : middle + 1;
 }
 
 function isiterable(v) {
-    return v?.[Symbol.iterator] != undefined;
+	return v?.[Symbol.iterator] != undefined;
 }
 
 function searchsorted(a, v, side = 'left') {
-    if (isiterable(v)) {
-        let result = [];
-        for (let n of v) {
-            result.push(searchsorted(a, n, side));
-        }
-        return result;
-    }
-    let i = binarysearch(a, v);
-    if (side == 'left') {
-        for (; i >= 0 && a[i - 1] == v; i--);
-    } else if (side == 'right') {
-        for (; i < a.length && a[i] == v; i++);
-    } else throw `unexpected argument side = ${side}`;
-    return i;
+	if (isiterable(v)) {
+		let result = [];
+		for (let n of v) {
+			result.push(searchsorted(a, n, side));
+		}
+		return result;
+	}
+	let i = binarysearch(a, v);
+	if (side == 'left') {
+		for (; i >= 0 && a[i - 1] == v; i--);
+	} else if (side == 'right') {
+		for (; i < a.length && a[i] == v; i++);
+	} else throw `unexpected argument side = ${side}`;
+	return i;
 }
 
 function* zip(...iterables) {
-    let iterators = iterables.map(iter => iter[Symbol.iterator]());
-    while (true) {
-        let any = false;
-        let result = [];
-        for (let i = 0; i < iterators.length; i++) {
-            let iter = iterators[i];
-            let v;
-            if (iter) {
-                let { value, done } = iter.next();
-                if (done) iterators[i] = null;
-                v = value;
-                any = true;
-            }
-            result.push(v);
-        }
-        if (!any) return;
-        yield result;
-    }
+	let iterators = iterables.map((iter) => iter[Symbol.iterator]());
+	while (true) {
+		let any = false;
+		let result = [];
+		for (let i = 0; i < iterators.length; i++) {
+			let iter = iterators[i];
+			let v;
+			if (iter) {
+				let { value, done } = iter.next();
+				if (done) iterators[i] = null;
+				v = value;
+				any = true;
+			}
+			result.push(v);
+		}
+		if (!any) return;
+		yield result;
+	}
 }
 
 let divide = (() => {
-    function divide_vv(a, b) {
-        let result = [];
-        for (let [n, m] of zip(a, b)) {
-            result.push(n / m);
-        }
-        return result;
-    }
-    function divide_vf(a, b) {
-        let result = [];
-        for (let n of a) {
-            result.push(n / b);
-        }
-        return result;
-    }
+	function divide_vv(a, b) {
+		let result = [];
+		for (let [n, m] of zip(a, b)) {
+			result.push(n / m);
+		}
+		return result;
+	}
+	function divide_vf(a, b) {
+		let result = [];
+		for (let n of a) {
+			result.push(n / b);
+		}
+		return result;
+	}
 
-    function divide_fv(a, b) {
-        let result = [];
-        for (let n of b) {
-            result.push(a / n);
-        }
-        return result;
-    }
+	function divide_fv(a, b) {
+		let result = [];
+		for (let n of b) {
+			result.push(a / n);
+		}
+		return result;
+	}
 
-    function divide_ff(a, b) {
-        return a / b;
-    }
+	function divide_ff(a, b) {
+		return a / b;
+	}
 
-    return function divide(a, b) {
-        let a_v = isiterable(a), b_v = isiterable(b);
-        if (a_v) {
-            if (b_v) return divide_vv(a, b);
-            return divide_vf(a, b);
-        }
-        if (b_v) return divide_fv(a, b);
-        return divide_ff(a, b);
-    }
+	return function divide(a, b) {
+		let a_v = isiterable(a),
+			b_v = isiterable(b);
+		if (a_v) {
+			if (b_v) return divide_vv(a, b);
+			return divide_vf(a, b);
+		}
+		if (b_v) return divide_fv(a, b);
+		return divide_ff(a, b);
+	};
 })();
 
 let subtract = (() => {
-    function subtract_vv(a, b) {
-        let result = [];
-        for (let [n, m] of zip(a, b)) {
-            result.push(n - m);
-        }
-        return result;
-    }
+	function subtract_vv(a, b) {
+		let result = [];
+		for (let [n, m] of zip(a, b)) {
+			result.push(n - m);
+		}
+		return result;
+	}
 
-    function subtract_vf(a, b) {
-        let result = [];
-        for (let n of a) {
-            result.push(n - b);
-        }
-        return result;
-    }
+	function subtract_vf(a, b) {
+		let result = [];
+		for (let n of a) {
+			result.push(n - b);
+		}
+		return result;
+	}
 
-    function subtract_fv(a, b) {
-        let result = [];
-        for (let n of b) {
-            result.push(a - n);
-        }
-        return result;
-    }
+	function subtract_fv(a, b) {
+		let result = [];
+		for (let n of b) {
+			result.push(a - n);
+		}
+		return result;
+	}
 
-    function subtract_ff(a, b) {
-        return a - b;
-    }
+	function subtract_ff(a, b) {
+		return a - b;
+	}
 
-    return function subtract(a, b) {
-        let a_v = isiterable(a), b_v = isiterable(b);
-        if (a_v) {
-            if (b_v) return subtract_vv(a, b);
-            return subtract_vf(a, b);
-        }
-        if (b_v) return subtract_fv(a, b);
-        return subtract_ff(a, b);
-    }
+	return function subtract(a, b) {
+		let a_v = isiterable(a),
+			b_v = isiterable(b);
+		if (a_v) {
+			if (b_v) return subtract_vv(a, b);
+			return subtract_vf(a, b);
+		}
+		if (b_v) return subtract_fv(a, b);
+		return subtract_ff(a, b);
+	};
 })();
 
 function broadcast_shapes(...shapes) {
-    let depth = 0;
-    for (let shape of shapes) {
-        depth = Math.max(depth, shape.length);
-    }
-    let out = [];
-    if (depth > 0) {
-        for (let i = 0; i < depth; i++) out[i] = 1;
-        for (let shape of shapes) {
-            for (let i = shape.length - 1, ind = depth - 1; i >= 0; i--, ind--) {
-                let dim = shape[i];
-                if (dim == 1) continue;
-                if (out[ind] == 1) out[ind] = dim;
-                else if (out[ind] != dim) throw 'shape mismatch';
-            }
-        }
-    }
-    return out;
+	let depth = 0;
+	for (let shape of shapes) {
+		depth = Math.max(depth, shape.length);
+	}
+	let out = [];
+	if (depth > 0) {
+		for (let i = 0; i < depth; i++) out[i] = 1;
+		for (let shape of shapes) {
+			for (let i = shape.length - 1, ind = depth - 1; i >= 0; i--, ind--) {
+				let dim = shape[i];
+				if (dim == 1) continue;
+				if (out[ind] == 1) out[ind] = dim;
+				else if (out[ind] != dim) throw 'shape mismatch';
+			}
+		}
+	}
+	return out;
 }
 
 function _shape(array) {
-    if (array.shape != undefined) return array.shape;
-    let shape = [];
-    while (array.length != undefined) {
-        shape.push(array.length);
-        if (array.length == 0) break;
-        array = array[0];
-    }
-    return shape;
+	if (array.shape != undefined) return array.shape;
+	let shape = [];
+	while (array.length != undefined) {
+		shape.push(array.length);
+		if (array.length == 0) break;
+		array = array[0];
+	}
+	return shape;
 }
 
 // function broadcast_to(array, shape) {
@@ -256,8 +255,6 @@ function _shape(array) {
 
 //     }
 // }
-
-
 
 // console.log(broadcast_to([1, 2, 3], [3, 3]));
 
@@ -270,64 +267,72 @@ function _shape(array) {
 // console.log(broadcast_shapes([6, 7], [5, 6, 1], [7], [5, 1, 7]));
 
 let clip = (() => {
-    function clip_fff(n, lower, upper) {
-        return Math.min(Math.max(n, lower), upper);
-    }
-    function clip_vff(n, lower, upper) {
-        return Math.min(Math.max(n, lower), upper);
-    }
+	function clip_fff(n, lower, upper) {
+		return Math.min(Math.max(n, lower), upper);
+	}
+	function clip_vff(n, lower, upper) {
+		return Math.min(Math.max(n, lower), upper);
+	}
 
-    return function clip(a, lower, upper) {
-        let a_v = isiterable(a), lower_v = isiterable(lower), upper_v = isiterable(upper);
-        if (a_v) {
-            return clip_vff(a, lower, upper);
-        }
-        return clip_fff(a, lower, upper);
-    }
+	return function clip(a, lower, upper) {
+		let a_v = isiterable(a),
+			lower_v = isiterable(lower),
+			upper_v = isiterable(upper);
+		if (a_v) {
+			return clip_vff(a, lower, upper);
+		}
+		return clip_fff(a, lower, upper);
+	};
 })();
 
 function argmin(a) {
-    let min_i = -1;
-    let min_v = Infinity;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] < min_v) {
-            min_i = i;
-            min_v = a[i];
-        }
-    }
-    return min_i;
+	let min_i = -1;
+	let min_v = Infinity;
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] < min_v) {
+			min_i = i;
+			min_v = a[i];
+		}
+	}
+	return min_i;
 }
 
 function argmax(a) {
-    let max_i = -1;
-    let max_v = -Infinity;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] > max_v) {
-            max_i = i;
-            max_v = a[i];
-        }
-    }
-    return max_i;
+	let max_i = -1;
+	let max_v = -Infinity;
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] > max_v) {
+			max_i = i;
+			max_v = a[i];
+		}
+	}
+	return max_i;
 }
 
 function gcd(a, b) {
-    return b == 0 ? a : gcd(b, a % b);
+	return b == 0 ? a : gcd(b, a % b);
 }
 
 function _clip_prob(p) {
-    return clip(p, 0.0, 1.0);
+	return clip(p, 0.0, 1.0);
 }
 
 function _select_and_clip_prob(cdfprob, sfprob, cdf = true) {
-    p = np.where(cdf, cdfprob, sfprob);
-    return _clip_prob(p);
+	p = np.where(cdf, cdfprob, sfprob);
+	return _clip_prob(p);
 }
 
-let data1 = [13, 10, 8, 11, 11, 6, 17, 16, 15, 17, 14, 14, 14, 12, 16, 20, 18, 17, 20, 15]
-let data2 = [9, 13, 6, 13, 7, 12, 11, 8, 9, 10, 9, 10, 4, 7, 12, 15, 4, 5, 9, 10, 7, 8, 9, 5, 18, 13, 12, 11, 7, 11, 10, 8, 8, 10, 10, 9, 7, 7, 10, 10, 10, 8, 9, 14, 9, 7, 8, 9, 10, 7, 11, 7, 7, 10, 12, 14, 13, 9, 6, 13, 14, 12, 5, 14, 9, 4, 13, 7, 6, 7, 8, 11, 9, 7, 14, 16, 8, 7, 8, 9, 15, 9, 12, 7, 10, 10, 7, 6, 8, 12, 7, 5, 3, 6, 14, 16, 12, 4, 11, 15, 8, 9, 4, 7, 9, 16, 13, 11, 10, 12, 7, 8, 10, 15, 12, 6, 15, 10, 11, 11, 9, 9, 9, 14, 7, 7, 14, 10, 13, 11, 15, 11, 13, 13, 9, 9, 13, 7, 14, 9, 8, 6, 12, 11, 14, 7, 13, 17, 11, 15, 12, 9, 17, 7, 6, 12, 12, 10, 11, 7, 10, 9, 9, 8, 12, 13, 10]
+let data1 = [13, 10, 8, 11, 11, 6, 17, 16, 15, 17, 14, 14, 14, 12, 16, 20, 18, 17, 20, 15];
+let data2 = [
+	9, 13, 6, 13, 7, 12, 11, 8, 9, 10, 9, 10, 4, 7, 12, 15, 4, 5, 9, 10, 7, 8, 9, 5, 18, 13, 12, 11, 7, 11, 10, 8, 8,
+	10, 10, 9, 7, 7, 10, 10, 10, 8, 9, 14, 9, 7, 8, 9, 10, 7, 11, 7, 7, 10, 12, 14, 13, 9, 6, 13, 14, 12, 5, 14, 9, 4,
+	13, 7, 6, 7, 8, 11, 9, 7, 14, 16, 8, 7, 8, 9, 15, 9, 12, 7, 10, 10, 7, 6, 8, 12, 7, 5, 3, 6, 14, 16, 12, 4, 11, 15,
+	8, 9, 4, 7, 9, 16, 13, 11, 10, 12, 7, 8, 10, 15, 12, 6, 15, 10, 11, 11, 9, 9, 9, 14, 7, 7, 14, 10, 13, 11, 15, 11,
+	13, 13, 9, 9, 13, 7, 14, 9, 8, 6, 12, 11, 14, 7, 13, 17, 11, 15, 12, 9, 17, 7, 6, 12, 12, 10, 11, 7, 10, 9, 9, 8,
+	12, 13, 10,
+];
 data1.sort((a, b) => a - b);
 data2.sort((a, b) => a - b);
-
 
 let n1 = data1.length;
 let n2 = data2.length;
@@ -352,26 +357,25 @@ let maxS = cddiffs.at(argmaxS);
 
 let d, d_location, d_sign;
 if (minS > maxS) {
-    d = minS;
-    d_location = loc_minS;
-    d_sign = -1;
+	d = minS;
+	d_location = loc_minS;
+	d_sign = -1;
 } else {
-    d = maxS;
-    d_location = loc_maxS;
-    d_sign = 1;
+	d = maxS;
+	d_location = loc_maxS;
+	d_sign = 1;
 }
 
 let g = gcd(n1, n2);
-let n1g = n1 / g | 0;
-let n2g = n2 / g | 0;
+let n1g = (n1 / g) | 0;
+let n2g = (n2 / g) | 0;
 let prob = -Infinity;
 
 let [m, n] = [+n1, +n2].sort((a, b) => b - a);
-let en = m * n / (m + n);
+let en = (m * n) / (m + n);
 // console.log(m, n, en)
 
 // // prob = sf(d, Math.round(en));
-
 
 // console.log('searchsorted', searchsorted(data1, data_all, 'right'), data1, data_all);
 
